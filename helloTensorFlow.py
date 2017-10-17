764a32567d74dd4384427a66a9d1d9930987c4cc
Shayan Ray <shayan.ray@gmail.com>
2017-10-16 23:33:31 -0400

    hello world in tensor-flow

--- helloTensorFlow.py
+++ helloTensorFlow.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
+
+# create a constant
 hello = tf.constant("Hello TensorFlow !!")
+
+# everything works out of a session in tf
 session = tf.Session()
+
+#print
 print(session.run(hello))
+
+
